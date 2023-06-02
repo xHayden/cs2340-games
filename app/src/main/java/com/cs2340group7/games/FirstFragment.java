1667e6f71e0b298270b4bb4b7c1863ee33d41483
Hayden Carpenter <haydencarp@outlook.com>
2023-06-02 16:36:49 -0400

    Adding first view support for play game menu

--- app/src/main/java/com/cs2340group7/games/FirstFragment.java
+++ app/src/main/java/com/cs2340group7/games/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +15,8 @@ import com.cs2340group7.games.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private TextView gameName;
+    private TextView description;
 
     @Override
     public View onCreateView(
@@ -22,7 +25,10 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View view = binding.getRoot();
+        gameName = (TextView) view.findViewById(R.id.gameName);
+        description = (TextView) view.findViewById(R.id.description);
+        return view;
 
     }
 
@@ -32,8 +38,10 @@ public class FirstFragment extends Fragment {
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                setGameName("Test");
+                setDescription("test2");
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
     }
@@ -42,6 +50,18 @@ public class FirstFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void setGameName(String text) {
+        if (gameName != null) {
+            gameName.setText(text);
+        }
+    }
+
+    public void setDescription(String text) {
+        if (description != null) {
+            description.setText(text);
+        }
     }
 
 }
--- app/src/main/java/com/cs2340group7/games/MainActivity.java
+++ app/src/main/java/com/cs2340group7/games/MainActivity.java
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        Log.d("Create Nav Controller", navController.getCurrentDestination()
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
@@ -43,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAnchorView(R.id.fab)
                         .setAction("Action", null).show();
+
             }
         });
     }
